a0814bc527a02e061c2ec4c9413256c163a629c3
Toan Nguyen <blackntt@gmail.com>
2019-11-25 21:04:35 +0700

    update file install.sh from appimage to debian

--- install.sh
+++ install.sh
@@ -2,17 +2,9 @@
 echo 'source $HOME/dotfiles/.bashrc' >> $HOME/.bashrc
 
 #install neovim
-curl -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-chmod u+x nvim.appimage
-mkdir -p $HOME/apps
-mv nvim.appimage $HOME/apps
-
-#create symbolic vim to neovim
-sudo apt remove vim
-cd $HOME/apps
-ln -sf nvim.appimage nvim
-ln -sf nvim.appimage vim
-echo 'export PATH=$HOME/apps:$PATH' >> $HOME/.bashrc
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
 
 #create file config for neovim
 cd $HOME/.config
